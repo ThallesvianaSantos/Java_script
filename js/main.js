928fb7be4c80efcb78ff8c66cb2539aded45b334
Thalles Viana <thalles.santos61@gmail.com>
2020-11-12 17:10:50 -0300

    aulas de java script, mod1

---
+++ js/main.js
@@ -0,0 +1,63 @@
+/*
+var nome = "thalles viana";
+var idade = 23;
+var idade2 = 10;
+var frase = "japão é o melhor time do mundo!"
+alert("bem vindo" + nome);
+alert(nome + " tem " + idade + " anos ");
+alert(idade+idade2);
+
+console.log(nome);
+console.log(idade2+idade);
+console.log(frase.replace("japão","brasil"));
+alert(frase.replace("japão","brasil"));
+*/
+
+var lista = ["maça","pera","laranja"];
+console.log(lista);
+lista.push("uva");
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.toString()[0]);
+console.log(lista.join("|"));
+lista.pop();
+console.log(lista);
+
+/*
+var fruta = [{nome: "maça", cor:"vermelha"},{nome: "uva", cor:"goxo"},{nome: "morango", cor:"vermelha"}]
+console.log(fruta[1].nome);
+alert(fruta[1].cor);
+*/
+/*
+var idade = prompt("qual a sua idade?");
+if(idade>=18);{
+    alert("maior de idade");
+};
+if(idade<10){
+    alert("menor de idade");
+};
+*/
+/*
+var count = 0;
+while(count<=5)
+{
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+/*
+var count;
+for(count=0;count<=5; count++)
+{
+    alert(count);
+}
+*/
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+*/
+
